8594722ac67a45395d34640d0213af3ed51d9e53
John Callahan <jcallahan@acm.org>
2017-11-24 11:35:48 -0500

    added description to quiz

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123232441) do
+ActiveRecord::Schema.define(version: 20171124163456) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20171123232441) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.integer  "numquestions", limit: 4
+    t.string   "description",  limit: 255
   end
 
   create_table "users", force: :cascade do |t|
