cf777562cac181bbab121f9beb6def6dba040f0b
John Callahan <jcallahan@acm.org>
2017-11-25 21:44:49 -0500

    added icon name to quizzes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124163456) do
+ActiveRecord::Schema.define(version: 20171126023751) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20171124163456) do
     t.datetime "updated_at",               null: false
     t.integer  "numquestions", limit: 4
     t.string   "description",  limit: 255
+    t.string   "iconname",     limit: 255
   end
 
   create_table "users", force: :cascade do |t|
